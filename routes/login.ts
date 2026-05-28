3c217027b13c29b07c7243dca2ace20bf627d470
Xora Fix Engine <fix@getxora.ai>
2026-05-28 23:20:44 +0000

    fix: Xora automated fix for finding d02e3168-9657-4322-b204-4d857ab354cc

--- routes/login.ts
+++ routes/login.ts
@@ -31,7 +31,13 @@ export function login () {
 
   return (req: Request, res: Response, next: NextFunction) => {
     verifyPreLoginChallenges(req) // vuln-code-snippet hide-line
-    models.sequelize.query(`SELECT * FROM Users WHERE email = '${req.body.email || ''}' AND password = '${security.hash(req.body.password || '')}' AND deletedAt IS NULL`, { model: UserModel, plain: true }) // vuln-code-snippet vuln-line loginAdminChallenge loginBenderChallenge loginJimChallenge
+    const email = req.body.email || ''
+    const password = req.body.password || ''
+    if (/['";\\]|--|\/\*|\*\//.test(email)) {
+      res.status(401).send(res.__('Invalid email or password.'))
+      return
+    }
+    models.sequelize.query('SELECT * FROM Users WHERE email = :email AND password = :password AND deletedAt IS NULL', { replacements: { email, password: security.hash(password) }, model: UserModel, plain: true })
       .then((authenticatedUser) => { // vuln-code-snippet neutral-line loginAdminChallenge loginBenderChallenge loginJimChallenge
         const user = utils.queryResultToJson(authenticatedUser)
         if (user.data?.id && user.data.totpSecret !== '') {
