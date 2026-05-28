745cab4f05654c06f8145686c464335d4c016078
Xora Fix Engine <fix@getxora.ai>
2026-05-28 17:53:43 +0000

    fix: Xora automated fix for finding 89489cac-d828-464a-83d6-6cef1762557b

--- routes/coupon.ts
+++ routes/coupon.ts
@@ -8,16 +8,21 @@ import { BasketModel } from '../models/basket'
 import * as security from '../lib/insecurity'
 
 export function applyCoupon () {
-  return async ({ params }: Request, res: Response, next: NextFunction) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const id = params.id
-      let coupon: string | undefined | null = params.coupon ? decodeURIComponent(params.coupon) : undefined
+      const id = req.params.id
+      let coupon: string | undefined | null = req.params.coupon ? decodeURIComponent(req.params.coupon) : undefined
       const discount = security.discountFromCoupon(coupon)
       coupon = discount ? coupon : null
 
       const basket = await BasketModel.findByPk(id)
       if (!basket) {
         next(new Error(`Basket with id=${id} does not exist.`))
+        return
+      }
+      const user = security.authenticatedUsers.from(req)
+      if (!user || basket.UserId !== user.data.id) {
+        res.status(403).send('Basket does not belong to user.')
         return
       }
 
